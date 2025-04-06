b4ace1cd0feb5d85e3290959435c8d7aeb177d7d
Egor Zhuravlev <mezhuravlevegor@gmail.com>
2025-04-06 10:17:43 +0300

    [ФЗ4] — Добавление классов задач всех типов

---
+++ src/models/Epic.java
@@ -0,0 +1,41 @@
+package models;
+
+import constants.TaskStatus;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Epic extends Task {
+    private List<Integer> subtaskIds;
+
+    public Epic(String title, String description) {
+        super(title, description, TaskStatus.NEW);
+        this.subtaskIds = new ArrayList<>();
+    }
+
+    public List<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
+
+    public void addSubtaskId(int subtaskId) {
+        subtaskIds.add(subtaskId);
+    }
+
+    public void removeSubtaskId(int subtaskId) {
+        subtaskIds.remove((Integer) subtaskId);
+    }
+
+    public void clearSubtaskIds() {
+        subtaskIds.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", subtaskIds=" + subtaskIds +
+                '}';
+    }
+}
---
+++ src/models/Subtask.java
@@ -0,0 +1,31 @@
+package models;
+
+import constants.TaskStatus;
+
+public class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(String title, String description, TaskStatus status, int epicId) {
+        super(title, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    public void setEpicId(int epicId) {
+        this.epicId = epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", epicId=" + epicId +
+                '}';
+    }
+}
